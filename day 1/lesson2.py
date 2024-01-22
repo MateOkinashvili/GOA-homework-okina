29854d95e2aadb84d79132e7fce9f140f303a85b
MateOkinashvili <mateokinashvili@gmail.com>
2024-01-23 00:06:26 +0400

    second commit

---
+++ day 1/lesson2.py
@@ -0,0 +1,9 @@
+name = "mate"
+
+surname = "okinashvili"
+
+age = 17
+
+height = 174.5
+
+print("me" " " "mqvia" + " " + name + " " + surname + "," " " "var" + " " + str(age) + " " + "wlis" + "." + " " "chemi" " " "simaglea" + " " + str(height) + "cm" " " "da" " " "minda" " " "avgnishno" " " "is" " " "rom" " " "goa-s" " " "axali" " " "wevri" " " "var" + ".")
