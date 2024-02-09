142ede66d176607d9893f7c731daf3e2b4f3c904
MateOkinashvili <mateokinashvili@gmail.com>
2024-02-09 15:34:35 +0400

    first commit

---
+++ day 2/lesson3.py
@@ -0,0 +1,5 @@
+print("gavicani vano motiashvili da nika keshelava")
+
+
+
+print("goa dzaan momewona uvke, momewona garemo, momewona tu rogor xsnis nika maswavlebeli, minda warmatebebi vusurvo nikas maswavlebels da mtlian goas")
