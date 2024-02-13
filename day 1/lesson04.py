2938202f80e3595a4933afa67ecccc8d6973854c
MateOkinashvili <mateokinashvili@gmail.com>
2024-02-13 17:50:12 +0400

    goa davaleba

---
+++ day 1/lesson04.py
@@ -0,0 +1,19 @@
+my_age = 17
+
+sister_age = 26
+
+brother_age = 18
+
+mom_age = 44
+
+dad_age = 55
+
+print(my_age + 2)
+
+print(sister_age + 2)
+
+print(brother_age + 2)
+
+print(mom_age + 2)
+
+print(dad_age + 2)
