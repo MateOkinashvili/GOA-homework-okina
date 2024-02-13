5db36861cb9d8316dfa65fdc698d628004b0ac80
MateOkinashvili <mateokinashvili@gmail.com>
2024-02-13 17:55:59 +0400

    second commit

--- day 1/lesson03.py
+++
@@ -1,5 +0,0 @@
-print("gavicani vano motiashvili da nika keshelava")
-
-
-
-print("goa dzaan momewona uvke, momewona garemo, momewona tu rogor xsnis nika maswavlebeli, minda warmatebebi vusurvo nikas maswavlebels da mtlian goas")
