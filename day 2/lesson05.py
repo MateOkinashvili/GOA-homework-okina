bce6e441210373413de8e7f690e92993b3cbdcd9
MateOkinashvili <mateokinashvili@gmail.com>
2024-02-20 20:22:21 +0400

    goa davaleba

--- day 2/lesson05.py
+++
@@ -1,7 +0,0 @@
-name = input(":Please enter your name:")
-
-print(name)
-
-surname = input(":please enter your surname:")
-
-print(surname)
---
+++ day 6/lesson06.py
@@ -0,0 +1,7 @@
+num1 = int(input(":enter number one: "))
+
+num2 = int(input(":enter number two: "))
+
+num3 = int(input(":enter number three: "))
+
+print("Here is your answer" + print (num1 * num2 * num3))
