f346e8849e9d987a3209b0e89740f5ff7be37976
MateOkinashvili <mateokinashvili@gmail.com>
2024-02-21 21:08:56 +0400

    Goa

---
+++ day 7/lesson07.py
@@ -0,0 +1,24 @@
+num1 = int(input(":please enter the first number: "))
+
+num2 = int(input(":please enter the second number: "))
+
+print("num1 < num2" , num1 < num2)
+
+print("num1 > num2" , num1 > num2)
+
+print("num1 <= num2" , num1 <= num2)
+
+print("num1 >= num2" , num1 >= num2)
+
+print("num1 == num2" , num1 == num2)
+
+print("num1 != num2" , num1 != num2)
+
+print("num1 + num2" , num1 + num2)
+
+print("num1 - num2" , num1 - num2)
+
+print("num1 * num2" , num1 * num2)
+
+print("num1 / num2" , num1 / num2)
+
