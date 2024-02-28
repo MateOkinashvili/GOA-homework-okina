1a859afa35a0d7d7ea5d239bb7b4defd3a03ed6c
MateOkinashvili <mateokinashvili@gmail.com>
2024-02-28 17:41:12 +0400

    davaleba

--- day 7/lesson07.py
+++ day 7/lesson07.py
@@ -2,23 +2,23 @@ num1 = int(input(":please enter the first number: "))
 
 num2 = int(input(":please enter the second number: "))
 
-print("num1 < num2" , num1 < num2)
+print(f"{num1} < {num2}" , num1 < num2)
 
-print("num1 > num2" , num1 > num2)
+print(f"{num1} > {num2}" , num1 > num2)
 
-print("num1 <= num2" , num1 <= num2)
+print(f"{num1} <= {num2}" , num1 <= num2)
 
-print("num1 >= num2" , num1 >= num2)
+print(f"{num1} >= {num2}" , num1 >= num2)
 
-print("num1 == num2" , num1 == num2)
+print(f"{num1} == {num2}" , num1 == num2)
 
-print("num1 != num2" , num1 != num2)
+print(f"{num1} != {num2}" , num1 != num2)
 
-print("num1 + num2" , num1 + num2)
+print(f"{num1} + {num2}" , num1 + num2)
 
-print("num1 - num2" , num1 - num2)
+print(f"{num1} - {num2}" , num1 - num2)
 
-print("num1 * num2" , num1 * num2)
+print(f"{num1} * {num2}" , num1 * num2)
 
-print("num1 / num2" , num1 / num2)
+print(f"{num1} / {num2}" , num1 / num2)
 
---
+++ day 7/lesson08.py
@@ -0,0 +1,9 @@
+name = input("enter your name: ")
+
+surname = input("enter your last name: ")
+
+salary = int(input("enter your salary: "))
+
+
+ 
+print(f"my name is {name} {surname} and my salary is {salary}")
