f035904dbeea072387eda83c7c941cea40071ab3
MateOkinashvili <mateokinashvili@gmail.com>
2024-03-02 20:58:18 +0400

    davaleba goa

---
+++ day 8/test.py
@@ -0,0 +1,7 @@
+
+your_age = int(input("please enter your age: "))
+
+if your_age >= (18):
+    print("you are adult")
+else:
+    print("you are not adult")
---
+++ day 9/lesson09.py
@@ -0,0 +1,9 @@
+name = (input("Please enter your name: "))
+password = (input("Please enter your password: "))
+repeat_password = (input("Please repeat your password: "))
+
+
+if repeat_password != password:
+    print("your password is incorrect! ")
+else:
+    print("Successfully Authorized")
