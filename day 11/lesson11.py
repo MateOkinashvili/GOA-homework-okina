b108eb7f7b8df2083279f30d72786e0810fb7777
MateOkinashvili <mateokinashvili@gmail.com>
2024-03-10 20:10:19 +0400

    davaleba

---
+++ day 11/lesson11.py
@@ -0,0 +1,38 @@
+#სიის სახით პროგრამირების ენების შემოტანა, დაპრიტვა მთლიანი სიის და დაპრინტვა ბოლო ელემენტის
+
+programming_languages = ["java", "python", "JavaScript", "html", "css"]
+
+print(programming_languages)
+print(programming_languages[4])
+
+###
+
+#ცვლადების შექმნა და მათი ცალ-ცალკე გამოტანა
+
+variables = ["string", 1, 1.43, "true"]
+print(variables[0])
+print(variables[1])
+print(variables[2])
+print(variables[3])
+
+###
+
+#სიის შექმნა სადაცაა 0-იდან 20-ის ჩათვლით 4-ის ჯერადები და უდიდესის გამოტანა
+
+numbers = [4, 8, 12, 16, 20]
+print(numbers[4])
+
+###
+
+#30-იდან 50-ის ჩათვლით კენტი რიცხვები და აქედან სამი უმცირესი რიცხვის ჯამი
+
+odd_numbers = [31, 33, 35, 37, 39, 41, 43, 45, 47, 49]
+print(odd_numbers[0] + odd_numbers[1] + odd_numbers[2])
+
+###
+
+#სამის ჯერადები წინა სიიდან
+
+print(odd_numbers[1], odd_numbers[4], odd_numbers[7])
+
+
