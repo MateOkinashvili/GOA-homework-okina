2addcee1acf5386541cda9f758c39bb966cf406f
MateOkinashvili <mateokinashvili@gmail.com>
2024-08-11 23:05:12 +0400

    dvlebbaba

---
+++ day 55/Homework/homework1.js
@@ -0,0 +1,7 @@
+let p = document.getElementById('txt')
+
+let name = prompt("chawere saxeli:")
+let surname = prompt("chawere gvari:")
+let age = prompt("chawere asaki:")
+
+p.textContent = "sheni saxelia " + name + ';' + " " + "sheni gvaria " + surname + ';' + " " + "sheni asakia " + age
